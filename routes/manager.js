7e9354cc5d4c740d71aab2ab92b42493df86a0cf
Shravan <shravan097@users.noreply.github.com>
2018-03-16 16:13:54 -0400

    Added Sign Up Data/ Cleaned Folders

--- routes/manager.js
+++ routes/manager.js
@@ -2,9 +2,10 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 const Manager = require("../models/managerSchema")
+const bcrypt = require("bcrypt");
 
 //Get all Manager
-router.get("/",(req,res,next) =>{
+router.get("/getAll",(req,res,next) =>{
 	Manager.find()
 	.exec()
 	.then(docs=>{
@@ -45,6 +46,47 @@ router.post("/",(req,res,next)=>
 		res.status(500).json({
 			error:err
 		});
+	});
+
+});
+
+// Manager Sign Up
+router.post("/signup",(req,res,next)=>
+{
+	Manager.find({email: req.body.email})
+	.exec()
+	.then(manager =>{
+		if(manager.length >= 1){
+			return res.status(409).json({
+				message:"User Exist!"
+			});
+		}else{
+			bcrypt.hash(req.body.password,10,(err,hash)=>
+			{
+				if(err){
+					return res.status(500).json({
+						error: err
+					});
+				}else{
+					const manager = new Manager({
+						_id: new mongoose.Types.ObjectId(),
+						email: req.body.email,
+						password: hash
+					});
+					manager.save()
+					.then(result=>{
+						console.log(result);
+						res.status(201).json({
+							message:"User Created!"});
+					})
+					.catch(err=>
+					{
+						console.log(err);
+						res.status(500).json({error:err});
+					});
+				}
+			});
+		}
 	});
 
 });
